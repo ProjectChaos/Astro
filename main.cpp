8d9510ba7c800f1ffb0c8d2027428ede68fd1e60
JohSpa <JohannesSpangenberg@gmx.de>
2013-09-16 14:52:39 +0200

    Signed-off-by: JohSpa <JohannesSpangenberg@gmx.de>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
+using std::cin;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    long long x;
+    cout << "geben sie eine Zahl von 0 bis 1000000000000 ein: ";
+    cin >>x;
+    long long y=x*x;
+    cout <<(x*x)<< endl;
+    cout << y << endl;
+
     return 0;
 }
