593cc5c1c0785859ad9257b647072c3a17bab99d
Philmaweb <philmaweb@gmail.com>
2013-09-17 14:21:38 +0200

    Ich will aber nicht!!! Signed-off-by: Philmaweb <philmaweb@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,16 +3,67 @@
 using namespace std;
 using std::cin;
 
+double abs(double c){
+if(c<0){
+    c= -1*c;
+}
+return c;
+}
+
+double power(int exponent, double val){
+double acc = 1;
+// ist exponent negativ?
+    if(exponent < 0){
+    acc = 1/ (power(-1*exponent, val));
+    }
+    else {
+        while (exponent>0){
+            acc = acc * val;
+            exponent --;
+        }
+}
+return acc;
+}
+//double wurzel (double a){
+//    double x;
+//    double y;
+//    x =1;
+//    double betrag;
+//    double abbruch = power (-7,10);
+//    while(abs(x-y)>=abbruch){
+//                x=y;
+//                y = ((x+a/x)/2);
+//
+//    }
+//    cout  << "wurzel von "<< a << " ist " << x;
+//    }
+
+    double wurzelInOut(){
+    double in;
+    cin >>in;
+
+    double x;
+    double y;
+    string punkte="";
+    x =1;
+    y = ((x+in/x)/2);
+    while(abs(x-y)>(power (-7,10) )){
+                x=y;
+                y = ((x+in/x)/2);
+            punkte +=".";
+            cout  << "Berechne "<< punkte << endl;
+    }
+    cout << "Ergebnis ist: " << x << endl;
+    return x;
+    }
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    long long x;
-    cout << "geben sie eine Zahl von 0 bis 1000000000000 ein: ";
-    cin >>x;
-    long long y=x*x;
-    cout <<(x*x)<< endl;
-    cout << y << endl;
-    cout <<"j"<< endl;
+wurzelInOut();
+
+
+
 
 
     return 0;
