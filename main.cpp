81790c1d9c2e04c7145a2366d2d4003541ee4ea5
JohSpa <JohannesSpangenberg@gmx.de>
2013-09-17 14:19:26 +0200

    Signed-off-by: JohSpa <JohannesSpangenberg@gmx.de>

--- main.cpp
+++ main.cpp
@@ -3,17 +3,76 @@
 using namespace std;
 using std::cin;
 
+
+
+double abs(double c){
+if(c<0){
+    c=-1*c;
+}
+return c;
+}
+double power(int exp, double val){
+    double acc=1;
+    if (exp<0){
+        acc=1/(power (-1*exp, val));
+    }
+    else {
+        while (exp>0){
+                acc=acc * val;
+        exp --;
+            }
+    }
+    return acc;
+}
+double wurzelinout(){
+    double a;
+    double x;
+    double y;
+    int j=1;
+    string komplex="";
+    cout << "*******************************************************"<< endl ;
+    cout << "*                                                     *"<< endl ;
+    cout << "*     WURZELBERECHNUNG MIT DEM HERON ALGORITHMUS      *" << endl;
+    cout << "*                                                     *"<< endl ;
+    cout << "*******************************************************"<< endl << endl;
+    cout << "Geben sie die zu wurzelnde Zahl ein:"<< endl;
+    cout << "a=";
+    cin >>a;
+    x=1;
+    y= (x+a/x)/2;
+    string punkte="";
+if (a<0){
+    a*=-1;
+    komplex="*i";
+}
+else {}
+     while (abs(x-y)>=power(-7,10)){
+        x=y;
+        y= (x+a/x)/2;
+        punkte+=".";
+        j++;
+        cout << "Berechne" << punkte << endl;
+    }
+cout << endl << "ERGEBNIS: Wurzel a=:  " << x << komplex <<endl;
+cout << endl << "Benoetigte Berechnungschritte= " << j << endl;
+cout << endl << "Genaugikeit:"  << abs(x-y) << endl;
+   return 0;
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    long long x;
-    cout << "geben sie eine Zahl von 0 bis 1000000000000 ein: ";
-    cin >>x;
-    long long y=x*x;
-    cout <<(x*x)<< endl;
-    cout << y << endl;
-    cout <<"j"<< endl;
 
+//    cout << "Hello world!" << endl;
+//    long long x;
+//    cout << "geben sie eine Zahl von 0 bis 1000000000000 ein: ";
+//    cin >>x;
+//    long long y=x*x;
+//    cout <<(x*x)<< endl;
+//    cout << y << endl;
+//
+//    cout <<"j"<< endl;
 
+wurzelinout();
+
+//        cout << power(-2,2);
     return 0;
 }
